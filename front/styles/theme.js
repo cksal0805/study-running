42aa4534aeafb8b52f27da49dd7af317edb3fcfd
Yoon <cksal0805>
2020-12-15 14:51:36 +0900

    Theme.js 에 size값 추가

--- front/styles/theme.js
+++ front/styles/theme.js
@@ -5,11 +5,26 @@ const color = {
   pink: '#F9637C',
   black: '#28364A',
   lightBlue: '#D4F0F0',
-  lightGrey: '#f3f3f4',
+  lightGrey: '#e5e5e5',
+};
+
+const size = {
+  mobile: '770px',
+  tabletS: '1023px',
+  tabletM: '1220px',
+  tabletL: '1280px',
+  laptop: '1460px',
+  desktop: '1700px',
 };
 
 const theme = {
   color,
+  mobile: `max-width: ${size.mobile}`,
+  tabletS: `max-width: ${size.tabletS}`,
+  tabletM: `max-width: ${size.tabletM}`,
+  tabletL: `max-width: ${size.tabletL}`,
+  laptop: `max-width: ${size.laptop}`,
+  desktop: `max-width: ${size.desktop}`,
 };
 
 export default theme;
